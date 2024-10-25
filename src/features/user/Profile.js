e0003ba542307cf1cfcfabf9056fd881cd0a73d3
NgoAnhKhai <anhkhaichannel@gmail.com>
2024-10-25 18:23:56 +0700

    code

--- src/features/user/Profile.js
+++ src/features/user/Profile.js
@@ -1,24 +1,24 @@
-import React from "react";
-import useAuth from "../../hooks/useAuth";
 import { Grid, Stack } from "@mui/material";
-import ProfileScoreCard from "./ProfileScoreCard";
 import ProfileAbout from "./ProfileAbout";
 import ProfileSocialInfo from "./ProfileSocialInfo";
 import PostForm from "../post/PostForm";
+import ProfileScorecard from "./ProfileScoreCard";
 import PostList from "../post/PostList";
+import useAuth from "../../hooks/useAuth";
 
 function Profile({ profile }) {
   const { user } = useAuth();
-
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} md={4}>
         <Stack spacing={3}>
-          <ProfileScoreCard profile={profile} />
+          <ProfileScorecard profile={profile} />
           <ProfileAbout profile={profile} />
           <ProfileSocialInfo profile={profile} />
         </Stack>
-        <Grid item xs={12} md={8}></Grid>
+      </Grid>
+
+      <Grid item xs={12} md={8}>
         <Stack spacing={3}>
           {user._id === profile._id && <PostForm />}
           <PostList userId={profile._id} />
